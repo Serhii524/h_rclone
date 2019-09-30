yparitcher/rclone

local: use lib/encoder

backend/local/encode_other.go
@@ -1,0 +1,9 @@
+//+build !windows
+
+package local
+
+import (
+	"github.com/ncw/rclone/fs/encodings"
+)
+
+const enc = encodings.LocalUnix

backend/local/encode_windows.go
@@ -1,0 +1,9 @@
+//+build windows
+
+package local
+
+import (
+	"github.com/ncw/rclone/fs/encodings"
+)
+
+const enc = encodings.LocalWindows

Serhii524/h_rclone

backend/local/encode_other.go
@@ -1,0 +1,9 @@
+//+build !windows
+
+package local
+
+import (
+	"github.com/rclone/rclone/fs/encodings"
+)
+
+const enc = encodings.LocalUnix

backend/local/encode_windows.go
@@ -1,0 +1,9 @@
+//+build windows
+
+package local
+
+import (
+	"github.com/rclone/rclone/fs/encodings"
+)
+
+const enc = encodings.LocalWindows